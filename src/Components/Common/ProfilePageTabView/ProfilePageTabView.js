31f046daac4c09ec45e96e31c05b1f875fe4a983
Alawode Emmanuel <alawodeemmanuel2@gmail.com>
2021-06-24 12:51:23 +0100

    oddFEELING

--- src/Components/Common/ProfilePageTabView/ProfilePageTabView.js
+++ src/Components/Common/ProfilePageTabView/ProfilePageTabView.js
@@ -1,22 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ProfilePageTabView.css';
 
 function ProfilePageTabView() {
-  /* payments_tab --- fleet_tab --- orders_tab --- ongoing_tab */
-  const Tab1 = 'payments_tab';
-  const Tab2 = 'fleet_tab';
-  const Tab3 = 'orders_tab';
-  const Tab4 = 'onngoing_tab';
+  /* All tabs
+  payments_tab --- 
+  fleet_tab --- 
+  orders_tab --- 
+  ongoing_tab --- 
+  */
+  const [Tab, setTab] = useState('orders_tab');
 
-  const selectedTab = Tab2;
+  // Create on click for payment tab
+  const Tab_1 = () => {
+    setTab('payments_tab');
+  };
+
+  //  Create on click for fleet tab
+  const Tab_2 = () => {
+    setTab('fleet_tab');
+  };
+
+  //  Create on click for orders tab
+  const Tab_3 = () => {
+    setTab('orders_tab');
+  };
+
+  //  Create on click for ongoingtab
+  const Tab_4 = () => {
+    setTab('ongoing_tab');
+  };
 
   return (
     <div className="tabViewContainer">
-      <div id={selectedTab}>
-        <p className="payment">Payment</p>
-        <p className="fleet">My Fleet</p>
-        <p className="orders">My Orders</p>
-        <p className="ongoing">Ongoing</p>
+      <div id={Tab}>
+        <p className="payment" onClick={Tab_1}>
+          Payment
+        </p>
+        <p className="fleet" onClick={Tab_2}>
+          My Fleet
+        </p>
+        <p className="orders" onClick={Tab_3}>
+          My Orders
+        </p>
+        <p className="ongoing" onClick={Tab_4}>
+          Ongoing
+        </p>
       </div>
     </div>
   );
